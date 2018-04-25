2e1ade4e05ea86f44f467987f40e8d250ba441f2
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-04-25 17:55:31 +0300

    Добавил средства тестирования

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+
+var foo = 'bar';
+
+describe('Truth', function () {
+    it('should be true', function () {
+        true.should.be.true
+    })
+  
+    it('should not be false', function () {
+        true.should.not.be.false
+    })
+})
