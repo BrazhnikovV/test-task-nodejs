0ebf7724e2529a612e28032d8b7d6b0aa64db8f0
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-04-26 16:33:38 +0300

    Добавлена зависимость ejs-locals, создан лэйаут приложения

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,6 +1,21 @@
+/**
+ * This is the 'home' action that is invoked
+ * when an action is not explicitly requested by users.
+ */
 exports.home = function (req, res) {
     res.render('pages/home', {
         title: 'Home page', 
         message: 'This is the "home" action of "pages" controller'
+    });
+};
+
+/**
+ * This is the 'about' action that is invoked
+ * when an action is not explicitly requested by users.
+ */
+exports.about = function (req, res) {
+    res.render('pages/about', {
+        title: 'About page', 
+        message: 'This is the "about" action of "pages" controller'
     });
 };
--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 var express = require( 'express' ),
+    ejsLocals = require('ejs-locals'),
     app = express(),
     pages = require( __dirname + '/controllers/pages' );
 
 // Поключаем механизм для работы с представлениями
+app.engine('ejs', ejsLocals)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
@@ -18,5 +20,7 @@ app.get( '/', function ( req, res ) {
 });
 
 app.get( '/home', pages.home );
+
+app.get( '/about', pages.about );
 
 module.exports = app;
