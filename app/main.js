d13e5f145ccadc2e8ac326eb98e296fe5212d6e1
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-04-26 15:27:37 +0300

    Добавлена зависимость шаблонизатора ejs, создано представление для экшена home

--- app/main.js
+++ app/main.js
@@ -1,7 +1,22 @@
-var express = require('express'), app = express();
+var express = require( 'express' ),
+    app = express(),
+    pages = require( __dirname + '/controllers/pages' );
 
-app.get('/', function (req, res) {
-    res.send('Hello, Express!')
-})
+// Поключаем механизм для работы с представлениями
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+// Подключаем папку для статики
+app.use(express.static( __dirname + '/public' ) );
+
+//app.get('/', function (req, res) {
+//    res.send('Hello, Express!')
+//});
+
+app.get( '/', function ( req, res ) { 
+    res.redirect( '/home' ); 
+});
+
+app.get( '/home', pages.home );
 
 module.exports = app;
