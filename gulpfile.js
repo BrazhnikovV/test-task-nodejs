ec79acc1adefb537326067b3b0bd6ef338bd08a1
vasek <vasek@valerian>
2018-04-29 01:04:10 +0300

    Сборка, сжатие изображений, сборка css

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,27 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
-//var sass = require('gulp-sass');
+var sass   = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+var newer  = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
 
 // Lint Task
-/*
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('public/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
-*/
 
 // Compile Our Sass
-/*
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('public/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css'));
 });
-*/
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
@@ -35,11 +34,29 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('public/dist/js'));
 });
 
+// Concatenate & Minify CSS
+gulp.task('css', function () {
+    gulp.src('public/css/*.css')
+        .pipe(concat('all.css'))
+        .pipe(cssmin())        
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest('public/dist/css'));
+});
+
+// image processing
+gulp.task('images', function() {
+    return gulp.src('public/images/*')
+      .pipe(newer('public/dist/images'))
+      .pipe(imagemin({ optimizationLevel: 2 }))
+      .pipe(gulp.dest('public/dist/images'));
+  });
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('public/js/*.js', [/*'lint',*/ 'scripts']);
-    //gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('public/js/*.js', ['lint', 'scripts']);
+    gulp.watch('public/scss/*.scss', ['sass']);
+    gulp.watch('public/images/*', ['images']);
 });
 
 // Default Task
-gulp.task('default', [/*'lint', 'sass',*/ 'scripts', 'watch']);
+gulp.task('default', ['lint', 'images', 'sass', 'scripts', 'css','watch']);
