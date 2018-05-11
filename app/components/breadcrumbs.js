c7b85ea1eb1f334ddcf35b1e5b5f134b14cb0b6b
vasek <vasek@valerian>
2018-05-11 23:48:51 +0300

    Рефакторинг хлебных крошек.

--- app/components/breadcrumbs.js
+++ app/components/breadcrumbs.js
@@ -2,68 +2,51 @@
 var express = require('express');
 var router = express.Router();
 
-//
+// Массив соответсвий названий крошек и url
 var breadcrumbs_names = {
     products: 'продукты',
-    users: 'пользователи',
-    about: 'о нас',
-    home: 'гланая',
-    add: 'добавить',
-    page: 'страница'
+    users:    'пользователи',
+    about:    'о нас',
+    home:     'гланая',
+    add:      'добавить',
+    page:     'страница'
 };
 
 /* GET all pages. */
 router.get('*',function (req, res, next) {
-    if ( req.path.search( /\.*.\./i ) === -1 ) {        
-        let parsed_url = req.path.split("/");
-        if ( parsed_url.length === 2 ) {
-            if ( parsed_url[1] === "" ) {
-                res.locals.breadcrumbs = ['<li class="breadcrumb-item active" aria-current="page"><i class="fas fa-home"></i>гланая</li>'];
-            }
-            else {
-                res.locals.breadcrumbs = {
-                    0 : '<li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i>гланая</a></li>',
-                    1 : '<li class="breadcrumb-item active" aria-current="page">' 
-                    + 
-                        breadcrumbs_names[parsed_url[1]] 
-                    + '</li>' 
-                };
-            }
-        }
+    if ( req.path.search( /\.*.\./i ) === -1 ) {  
 
-        if ( parsed_url.length === 3 ) {
-            res.locals.breadcrumbs = {
-                0 : '<li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i>гланая</a></li>',
-                1 : '<li class="breadcrumb-item"><a href="/' 
-                + 
-                    parsed_url[1] + '">' + breadcrumbs_names[parsed_url[1]] 
-                + '</a></li>',
-                2 : '<li class="breadcrumb-item active" aria-current="page">' 
-                + 
-                    breadcrumbs_names[parsed_url[2]]
-                + '</li>'
-            };
-        }
+        var array_split = req.path.split("/");
+        array_split.splice(0, 1);
+        let array_urls = array_split;
 
-        if ( parsed_url.length === 4 ) {
-            res.locals.breadcrumbs = {
-                0 : '<li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i>гланая</a></li>',
-                1 : '<li class="breadcrumb-item"><a href="/' 
-                + 
-                    parsed_url[1]
-                + '">' 
-                + 
-                    breadcrumbs_names[parsed_url[1]]
-                + '</a></li>',
-                2 : '<li class="breadcrumb-item active" aria-current="page">' 
-                + 
-                    breadcrumbs_names[parsed_url[2]]
-                + '</li>',
-                3 : '<li class="breadcrumb-item active" aria-current="page">' 
-                + 
-                    parsed_url[3]
-                + '</li>'
-            };
+        res.locals.breadcrumbs = [];
+        
+        if ( array_urls[0] === '' ) {
+            res.locals.breadcrumbs.push({ link: false, name: 'home' });
+        }
+        else {
+            for ( let i = 0; i < array_urls.length; i++ ) {
+
+                var name = '';
+                var link = false;
+
+                if ( Number.isInteger(parseInt(array_urls[i])) ) {
+                    name = array_urls[i];
+                }
+                else {
+                    name = breadcrumbs_names[array_urls[i]];
+                }
+                
+                if ( array_urls.hasOwnProperty( i + 1 ) ) {                    
+                    if ( array_urls[i] === 'page' ) {
+                        continue;
+                    }   
+                    link = true;                                       
+                }
+                res.locals.breadcrumbs.push({ link: link, name: name, url: '/' + array_urls[i] });
+            }    
+            res.locals.breadcrumbs.unshift({ link: true, name: 'home', url: '/' });                        
         }
     }
     next();
